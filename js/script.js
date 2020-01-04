b5f1d752c836f4e4f47d448341d8546c9ebc9ad9
Mark Tiddy <marktiddy@gmail.com>
2020-01-04 17:34:02 +0000

    New javascript for menu and form validation

--- js/script.js
+++ js/script.js
@@ -25,8 +25,73 @@ $(document).ready(function() {
       );
     } // End if
   });
+
+  //some code for the menu classes
+  $(".navigation-list__item").click(function(e) {
+    var listOfItems = $(".navigation-list__item");
+
+    listOfItems.removeClass("navigation-list__item--active");
+
+    if ($(this).hasClass("navigation-list__item--active")) {
+      $(this).removeClass("navigation-list__item--active");
+    } else {
+      $(this).addClass("navigation-list__item--active");
+    }
+  });
 });
 
+//Code to update menu classes as we scroll
+$("#home").waypoint(
+  function(direction) {
+    $(".navigation-list__item").removeClass("navigation-list__item--active");
+    $("#home-button").addClass("navigation-list__item--active");
+  },
+  {
+    offset: "-15%"
+  }
+);
+
+$("#about").waypoint(function(direction) {
+  $(".navigation-list__item").removeClass("navigation-list__item--active");
+  $("#about-button").addClass("navigation-list__item--active");
+});
+
+$("#portfolio").waypoint(function(direction) {
+  $(".navigation-list__item").removeClass("navigation-list__item--active");
+  $("#portfolio-button").addClass("navigation-list__item--active");
+});
+
+$("#contact").waypoint(
+  function(direction) {
+    $(".navigation-list__item").removeClass("navigation-list__item--active");
+    $("#contact-button").addClass("navigation-list__item--active");
+  },
+  {
+    offset: "45%"
+  }
+);
 //form Validation
+
+$("#input-submit").on("click", function(event) {
+  event.preventDefault();
+
+  //Validation
+  inputValidation("name");
+  inputValidation("email");
+  inputValidation("message");
+});
+
+//Function to validate
+function inputValidation(field) {
+  if ($(`#input-${field}`).val()) {
+    $(`#${field}-error`)
+      .hide()
+      .fadeOut();
+  } else {
+    $(`#${field}-error`)
+      .fadeIn()
+      .show();
+  }
+}
 
 //To write!
